ffe0e6f81d4e0cbbb79cd82d75f6e11a17a98db1
rebe7917 <rebe7917@stud.kea.dk>
2022-11-09 11:26:10 +0100

    5carttotal works

--- src/components/Basket.jsx
+++ src/components/Basket.jsx
@@ -1,17 +1,25 @@
-
-
 function Basket(props) {
+  function getTotal() {
+    let total = 0;
+    props.cart.forEach((item) => {
+      total += item.amount * item.price;
+    });
+    return total;
+  }
   return (
     <section className="Basket">
       <ul>
         {props.cart.map((item) => (
-          <li>{item.productdisplayname} x {item.amount}, {item.amount * item.price},- </li>
+          <li>
+            {item.productdisplayname} x {item.amount}, {item.amount * item.price},-{" "}
+          </li>
         ))}
         {/*  <li>some product x 3</li> */}
       </ul>
+      <h3>Total: {getTotal()},-</h3>
       <button>Buy now</button>
     </section>
   );
 }
 
-export default Basket
+export default Basket;
